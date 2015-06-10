9b5c3cc2897a3771974e38a407afd119eeee980f
Glen Maddern <glenmaddern@gmail.com>
2015-06-10 19:10:45 +1000

    reusing elements by ID, so order in the DOM is preserved

--- index.js
+++ index.js
@@ -14,25 +14,26 @@ class CSSLoader {
   }
 
   fetch( load, fetch ) {
-    let deps = this._deps[load.metadata.pluginArgument] = []
+    let path = load.metadata.pluginArgument,
+      deps = this._deps[path] = []
     // Use the default Load to fetch the source
     return fetch( load ).then( source => {
       // Pass this to the CSS Modules core to be translated
       // triggerImport is how dependencies are resolved
-      return this.core.load( source, load.metadata.pluginArgument, "A", this.triggerImport.bind( this ) )
+      return this.core.load( source, path, "A", this.triggerImport.bind( this ) )
     } ).then( ( { injectableSource, exportTokens } ) => {
       if ( BUILD_MODE ) {
-        this._cache["./" + load.metadata.pluginArgument] = exportTokens
+        this._cache["./" + path] = exportTokens
         this._cache._source.push( injectableSource )
         return `export default ${JSON.stringify( exportTokens )}`
       } else {
         // Once our dependencies are resolved, inject ourselves
+        this.createElement( injectableSource, path )
         // And return out exported variables.
-        let id = this.createElement( injectableSource ),
-          imports = deps.map(d => `import "${d}"`).join(";"),
-          hotReloader = `export let __hotReload = () => {document.getElementById('${id}').remove(); return true;};`,
+        let imports = deps.map( d => `import "${d}"` ).join( ";" ),
+          hotReloader = `export let __hotReload = true`,
           exports = `export default ${JSON.stringify( exportTokens )}`
-        return [imports, hotReloader, exports].join(";")
+        return [imports, hotReloader, exports].join( ";" )
       }
     } )
   }
@@ -40,24 +41,34 @@ class CSSLoader {
 // Uses a <link> with a Blob URL if that API is available, since that
 // has a preferable debugging experience. Falls back to a simple <style>
 // tag if not.
-  createElement( source ) {
+  createElement( source, path ) {
     let head = document.getElementsByTagName( 'head' )[0],
-      cssElement
+      id = `jspm-css-loader-${path}`,
+      cssElement = document.getElementById(id)
 
+    // If we don't support Blob URLs, use a <style> tag
     if ( !window.Blob || !window.URL || !URL.createObjectURL || navigator.userAgent.match( /phantomjs/i ) ) {
-      cssElement = document.createElement( 'style' )
+      if (!cssElement) {
+        cssElement = document.createElement( 'style' )
+        cssElement.setAttribute( 'id', id )
+        head.appendChild( cssElement )
+      }
       cssElement.innerHTML = source
     } else {
+      if (!cssElement) {
+        cssElement = document.createElement( 'link' )
+        cssElement.setAttribute( 'id', id )
+        cssElement.setAttribute( 'rel', 'stylesheet' )
+        head.appendChild( cssElement )
+      } else {
+        URL.revokeObjectURL( cssElement.getAttribute( 'href' ) )
+      }
       let blob = new Blob( [source], { type: 'text/css' } ),
         url = URL.createObjectURL( blob )
 
-      cssElement = document.createElement( 'link' )
       cssElement.setAttribute( 'href', url )
-      cssElement.setAttribute( 'rel', 'stylesheet' )
     }
-    let id = `jspm-css-loader-${numElems++}`
-    cssElement.setAttribute( 'id', id )
-    head.appendChild( cssElement )
+
     return id
   }
 
@@ -66,7 +77,7 @@ class CSSLoader {
     // and trigger the import (which will instantiate this loader once more)
     let newPath = _newPath.replace( /^["']|["']$/g, "" ),
       rootRelativePath = "." + path.resolve( path.dirname( relativeTo ), newPath )
-    this._deps[relativeTo.replace(/^\//,'')].push(`${newPath}!${this.moduleName}`)
+    this._deps[relativeTo.replace( /^\//, '' )].push( `${newPath}!${this.moduleName}` )
     return System.import( `${rootRelativePath}!${this.moduleName}` ).then( exportedTokens => {
       // If we're in BUILD_MODE, the tokens aren't actually returned,
       // but they have been added into our cache.
