1cd6466022d6cb46c6db15e815cd30e7d9b8f079
Yifeng <f15gdsy@gmail.com>
2015-09-16 17:18:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// let fetch;
-// let translate;
-// let bundle;
-//
-// if (typeof window !== 'undefined') {
-//   fetch = load => {
-//     return System.import('./css-inject',  { name: __moduleName })
-//       .then(inject => inject.default(load));
-//   };
-// } else {
-//   // setting format = 'defined' means we're managing our own output
-//   translate = load => {
-//     load.metadata.format = 'defined';
-//   };
-//   bundle = function bundler(loads, opts) {
-//     return System.import('./css-builder', { name: __moduleName })
-//       .then(builder => builder.call(System, loads, opts));
-//   };
-// }
-//
-// export {fetch, translate, bundle};
-
 import Core from 'css-modules-loader-core'
 import path from 'path'
 import autoprefixer from './autoprefixer'
