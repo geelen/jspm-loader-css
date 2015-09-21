d57a10115d1d5855d1bf95caa703747818997287
Glen Maddern <glenmaddern@gmail.com>
2015-09-21 10:30:34 +0200

    ugh

---
+++ examples/config.js
@@ -0,0 +1,37 @@
+System.config({
+  "baseURL": "/",
+  "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime"
+    ]
+  },
+  "paths": {
+    "*": "*.js",
+    "github:*": "examples/jspm_packages/github/*.js",
+    "npm:*": "examples/jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "babel": "npm:babel-core@5.6.18",
+    "babel-runtime": "npm:babel-runtime@5.6.18",
+    "core-js": "npm:core-js@0.9.18",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:babel-runtime@5.6.17": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:babel-runtime@5.6.18": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@0.9.18": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    }
+  }
+});
+
