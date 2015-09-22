94cfb924d9db60dcbe467c31c91dcc0fb5ea50d4
Glen Maddern <glenmaddern@gmail.com>
2015-09-22 11:58:28 +0200

    add the @value keyword by default

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import {Plugins} from './lib/plugins'
 import {CSSLoader} from './lib/CSSLoader'
 
 const {fetch, bundle} = new CSSLoader([
+  Plugins.values,
   Plugins.extractImports,
   Plugins.scope,
   Plugins.autoprefixer()
